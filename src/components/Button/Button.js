a4f5fb0fa6373d804ec2045ea15bec51c4353f5a
Kirill <kirillplusena@gmail.com>
2021-03-03 22:21:37 +0300

    feat: create build

--- src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Button = (props) => {
-  const { onClick, className } = props;
-
-  return <button className={className} onClick={onClick}>{props.title}</button>;
-};
-export default Button;
